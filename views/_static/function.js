915cdd6dd14b08e95c975de6289745c3c76ef03a
damoncard <cardtergt@hotmail.com>
2017-05-15 00:20:37 +0700

    Add countdown animation

--- views/_static/function.js
+++ views/_static/function.js
@@ -1,3 +1,23 @@
+function createCountdown() {
+	$('#container').append(`<div id="wrap">
+					<div class="c"></div>
+					<div class="o"></div>
+					<div class="u"></div>
+					<div class="n"></div>
+					<div class="t"></div>
+			    </div>
+			<svg>
+				<defs>
+					<filter id="filter">
+						<feGaussianBlur in="SourceGraphic" stdDeviation="18" result="blur" />
+						<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 28 -10" result="filter" />
+						<feComposite in="SourceGraphic" in2="filter" operator="atop" />
+					</filter>
+				</defs>
+			</svg>`)
+	$('#wrap').hide()
+}
+
 function checkReady(list) {
 	for (var s in list) {
 		if (!list[s]) {
